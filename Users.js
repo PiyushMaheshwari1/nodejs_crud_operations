cd4e08532946710030ff7d514e2bc369619c9104
PiyushMaheshwari1 <62086889+PiyushMaheshwari1@users.noreply.github.com>
2021-03-25 13:33:57 +0530

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users= [
+{
+    id :1,
+    name:"jhon",
+    email: "jhon@gmail.com"
+},
+{
+    id :2,
+    name:"amar",
+    email: "amar@gmail.com"
+},
+{
+    id :3,
+    name:"c",
+    email: "c@gmail.com"
+},
+{
+    id :4,
+    name:"d",
+    email: "d@gmail.com"
+},
+];
+
+module.exports = users;
